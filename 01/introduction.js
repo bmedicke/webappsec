a4456b17d5d48aedaa9bf38addc865bf64c7e9d4
Benjamin Medicke <bmedicke@gmail.com>
2021-11-10 18:18:26 +0100

    nvm

---
+++ 01/introduction.js
@@ -0,0 +1,28 @@
+// dynamic
+
+let name = "value";
+const konstante = 42;
+
+
+// kommandostrukturen
+
+if (konstante == 42) {
+  // wenn true
+} else {
+  // wenn false
+}
+
+// arras, maps
+
+let array = [1, 2, 3];
+array.push(4);
+array.push("4");
+
+let maps = new Map();
+maps.set("key", "value");
+maps.get("key");
+
+for (let i of array) {
+  console.log("wert: " + i);
+  console.log()
+}
