055a8c1f759b8d6627bfec9ce6a2078c6c9fba23
root <bmedicke@gmail.com>
2021-12-09 10:41:49 +0100

    move script to playground, switch to coords

--- playground/sqla-engine.py
+++ playground/sqla-engine.py
@@ -16,27 +16,31 @@ engine = create_engine(
 create_db = """
 DROP TABLE IF EXISTS test;
 CREATE TABLE test (
-  number INTEGER NOT NULL
+  x INTEGER NOT NULL,
+  y INTEGER NOT NULL
 );
 """
 
-insert_numbers = """
-INSERT INTO test (number)
-VALUES (:number);
+insert_coords = """
+INSERT INTO test (x, y)
+VALUES (:x, :y);
 """
 
 select_all = """
 SELECT * from test;
 """
 
-numbers = [{"number": 42}, {"number": 8}, {"number": 21}]
+coords = [{"x": 42, "y": 0}, {"x": 8, "y": 13}, {"x": 21, "y": -2}]
 
 # autocommit block:
 # "begin once"
 with engine.connect() as conn:
     conn.execute(text(create_db))
-    conn.execute(text(insert_numbers), numbers)
+    conn.execute(text(insert_coords), coords)
     result = conn.execute(text(select_all))
     rows = result.all()
-    for row in rows:
-        from ptpython.repl import embed; embed(globals(), locals())
+    for x, y in rows:
+        print(f"x: {x}, y: {y}")
+    embed(globals(), locals())
+    from ptpython.repl import embed
+
