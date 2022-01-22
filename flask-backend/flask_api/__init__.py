606f680d1ec9d571fadb2c3a2b5ce3b937695a3a
root <bmedicke@gmail.com>
2022-01-22 08:41:48 +0100

    clean up

--- flask-backend/flask_api/__init__.py
+++ flask-backend/flask_api/__init__.py
@@ -30,31 +30,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # @app.route("/u/<user_id>")
-    # def user_profile(user_id):
-    #     """
-    #     user profile page
-    #     """
-    #     return f"{escape(user_id)}'s profile"
-
-    # @app.route("/e/<escaped>")
-    # def no_injection(escaped):
-    #     """
-    #     injection-safe demo route
-
-    #     localhost:7701/e/<body onload='alert("this is bad");'>
-    #     """
-    #     return f"{escape(escaped)}"
-
-    # @app.route("/i/<unescaped>")
-    # def injection(unescaped):
-    #     """
-    #     injection demo route
-
-    #     localhost:7701/i/<body onload='alert("this is bad");'>
-    #     """
-    #     return f"{unescaped}"
-
     # register database functions with the app:
     from . import database
 
